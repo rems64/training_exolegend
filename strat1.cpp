4c2dddc644c60e96be409159f9109e81d9098831
Remy Fayet <remy.fayet@telecom-paris.fr>
2024-10-26 23:37:50 +0200

    add dangerous grid cells (doesn't work yet)

--- strat1.cpp
+++ strat1.cpp
@@ -1,23 +1,29 @@
 #include <algorithm>
+#include <cmath>
 #include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <map>
+#include <memory>
 #include <string>
 #include <utility>
 #include <vector>
 
 using namespace std;
-constexpr int width = 13;
-constexpr int height = 11;
+constexpr unsigned int width = 13;
+constexpr unsigned int height = 11;
 
 enum CellType { Free, Crane, Wall, Enemy };
 
 struct CellInfo {
-    enum CellType type;
-    int crane_value;
+    enum CellType type = Free;
+    int crane_value = 0;
+    bool mined = false;
+    int mine_turn_count = 0;
+    bool dangerous = false;
+    int dangerous_turn_count = 0;
 };
 
 CellInfo make_cell_info(const char c) {
@@ -52,6 +58,7 @@ const char cell_to_char(CellInfo cell) {
 typedef std::map<std::pair<size_t, size_t>, CellInfo> grid_t;
 typedef int field_value_t;
 typedef field_value_t field_t[height][width];
+typedef std::pair<uint32_t, uint32_t> position_t;
 
 void print_grid(grid_t &grid) {
     for (size_t i = 0; i < height; i++) {
@@ -108,7 +115,8 @@ size_t flood_fill(field_t &field, grid_t &grid, field_t &seen,
     if (seen[i][j] > 0)
         return component_count;
     seen[i][j] = 1;
-    if (get_cell_info(grid, i, j).type != free_type) {
+    CellInfo cell = get_cell_info(grid, i, j);
+    if (cell.type != free_type || cell.dangerous) {
         field[i][j] = -1;
         return component_count;
     }
@@ -137,16 +145,122 @@ size_t compute_components(field_t &components, grid_t &grid) {
     return component_count;
 }
 
+uint32_t clamp_i(uint32_t i) { return max(min(i, height - 1), 0u); }
+
+uint32_t clamp_j(uint32_t j) { return max(min(j, width - 1), 0u); }
+
+void flag_bomb(grid_t &grid, uint32_t i, uint32_t j, int turns_left,
+               int range) {
+    CellInfo &cell = grid.at(std::make_pair(i, j));
+    cell.mined = true;
+    cell.mine_turn_count = turns_left;
+    for (size_t row = clamp_i(i - range); row < clamp_i(i + range - 1) + 1;
+         i++) {
+        for (size_t col = clamp_i(j - range); col < clamp_j(j + range - 1) + 1;
+             i++) {
+            CellInfo &endangered_cell = grid.at(std::make_pair(row, col));
+            endangered_cell.dangerous = true;
+            endangered_cell.dangerous_turn_count = turns_left;
+        }
+    }
+}
+
+position_t make_position(uint32_t i, uint32_t j) { return make_pair(i, j); }
+
+enum State { Grind };
+enum Action { Goto, Plant };
+
+bool reached(position_t &position, position_t &target) {
+    return position.first == target.first && position.second == target.second;
+}
+
+size_t distance(position_t &a, position_t &b) {
+    return abs((int)b.first - (int)a.first) +
+           abs((int)b.second - (int)a.second);
+}
+
+void pick_new_target(grid_t &grid, field_t &components, position_t &position,
+                     position_t &target) {
+    field_value_t current_value = components[position.first][position.second];
+    position_t best = position;
+    size_t best_distance = 0;
+    for (size_t i = 0; i < height; i++) {
+        for (size_t j = 0; j < width; j++) {
+            field_value_t v = components[i][j];
+            if (v == current_value) {
+                position_t candidate_position = make_position(i, j);
+                size_t d = distance(position, candidate_position);
+                if (d > best_distance) {
+                    best_distance = d;
+                    best = candidate_position;
+                }
+            }
+        }
+    }
+    cerr << "best candidate for new position is " << best.first << ", "
+         << best.second << endl;
+    target = best;
+}
+
+void pick_safe_actual_target(grid_t &grid, field_t &components,
+                             position_t &position, position_t &target,
+                             position_t &actual_target) {
+    field_value_t current_value = components[position.first][position.second];
+    position_t best = target;
+    size_t best_distance = 1000;
+    for (size_t i = 0; i < height; i++) {
+        for (size_t j = 0; j < width; j++) {
+            field_value_t v = components[i][j];
+            if (v != current_value)
+                continue;
+            position_t candidate_position = make_position(i, j);
+            if (grid.at(candidate_position).dangerous)
+                continue;
+            size_t d = distance(target, candidate_position);
+            if (d < best_distance) {
+                best_distance = d;
+                best = candidate_position;
+            }
+        }
+    }
+    actual_target = best;
+}
+
+void grind(grid_t &grid, field_t &components, enum State &state,
+           enum Action &action, position_t &position, position_t &target,
+           position_t &actual_target, bool &plant) {
+    if (reached(position, target)) {
+        switch (action) {
+        case Plant:
+            plant = true;
+        case Goto:
+            action = Plant;
+            pick_new_target(grid, components, position, target);
+        }
+    }
+    // pick_safe_actual_target(grid, components, position, target,
+    // actual_target);
+    actual_target = target;
+}
+
 int main() {
     int my_id;
     int given_width, given_height;
     cin >> given_width >> given_height >> my_id;
     cin.ignore();
+    position_t my_position;
 
+    enum State state = Grind;
+    enum Action action = Goto;
     grid_t grid;
-
+    uint32_t turn = 0;
+    uint32_t last_action_turn = -1000;
+    int drop_right = 1;
+    position_t target = {0, 0};
+    position_t actual_target = {0, 0};
     // game loop
     while (1) {
+        turn += 1;
         for (size_t i = 0; i < height; i++) {
             string row;
             cin >> row;
@@ -156,13 +270,6 @@ int main() {
             }
             cin.ignore();
         }
-        print_grid(grid);
-
-        field_t components;
-        size_t component_count = compute_components(components, grid);
-
-        cerr << "there are " << component_count << " components" << endl;
-        print_field(components);
 
         int entities;
         cin >> entities;
@@ -175,7 +282,37 @@ int main() {
             int param_1;
             int param_2;
             cin >> entity_type >> owner >> x >> y >> param_1 >> param_2;
+            switch (entity_type) {
+            // Player
+            case 0:
+                if (owner == my_id) {
+                    my_position = std::make_pair(y, x);
+                }
+                break;
+            // Bomb
+            case 1:
+                flag_bomb(grid, y, x, param_1, param_2);
+            }
             cin.ignore();
+        }
+
+        field_t components;
+        size_t component_count = compute_components(components, grid);
+        bool plant = false;
+
+        switch (state) {
+        case Grind:
+            grind(grid, components, state, action, my_position, target,
+                  actual_target, plant);
+            break;
+        }
+
+        if (plant) {
+            cout << "BOMB " << actual_target.second << " "
+                 << actual_target.first << endl;
+        } else {
+            cout << "MOVE " << actual_target.second << " "
+                 << actual_target.first << endl;
         }
 
         // cout << "BOMB 6 5" << endl;
